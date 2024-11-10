da0ae5b8cfa8fd473933fc3c29d14204ecd1c3ab
Anthony Guimard <bonjour@anthonyguimard.fr>
2024-11-10 12:43:31 +0100

    chore: add umami

--- app/root.tsx
+++ app/root.tsx
@@ -29,9 +29,21 @@ export default function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Noto+Serif:wght@300;400;600;700&display=swap" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Noto+Serif:wght@300;400;600;700&display=swap"
+        />
         <Links />
+        <script
+          defer
+          src="https://umami.tonours.fr/script.js"
+          data-website-id="a44eecf9-16e4-434e-8e2f-34ea3fc35668"
+        ></script>
       </head>
       <body>
         <Outlet />
